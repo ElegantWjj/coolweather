1a56da832c6a34930994ba57a35f05b6bf1b50c1
WeiJunJie <weijunjie1023@gmail.com>
2019-07-10 11:38:11 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/wjj/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/wjj/coolweather/util/HttpUtil.java
@@ -1,5 +1,8 @@
 package com.wjj.coolweather.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by Android Studio
  * #Author： 魏军杰
@@ -7,4 +10,13 @@ package com.wjj.coolweather.util;
  * #Time：9:05
  */
 public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
 }
--- app/src/main/java/com/wjj/coolweather/util/Utility.java
+++ app/src/main/java/com/wjj/coolweather/util/Utility.java
@@ -1,5 +1,15 @@
 package com.wjj.coolweather.util;
 
+import android.text.TextUtils;
+
+import com.wjj.coolweather.db.City;
+import com.wjj.coolweather.db.County;
+import com.wjj.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Android Studio
  * #Author： 魏军杰
@@ -7,4 +17,84 @@ package com.wjj.coolweather.util;
  * #Time：9:07
  */
 public class Utility {
+
+    /**
+     * 解析和处理服务器返回的省级数据
+     *
+     * @param response 从服务器请求到的数据
+     * @return
+     */
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析并处理服务器返回的市级数据
+     *
+     * @param response   从服务器请求到的数据
+     * @param provinceId 省份ID
+     * @return
+     */
+    public static boolean handlerCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析并处理服务器返回的县级数据
+     *
+     * @param response 从服务器请求到的数据
+     * @param cityId   城市ID
+     * @return
+     */
+    public static boolean handlerCountyResponse(String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length(); i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
